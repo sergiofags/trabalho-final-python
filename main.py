84ff152768418c96ae185563bd9318b45109fe8a
Sérgio Fagundes <127923437+sergiofags@users.noreply.github.com>
2025-01-21 21:17:07 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,146 +1,104 @@
 from enum import Enum
 from typing import List, Optional
+from datetime import datetime
 
-class SalaStatus(Enum):
-    ATIVA = 'Ativa'
-    INATIVA = 'Inativa'
-
-# Classes Principais
 class Usuario:
-    def __init__(self, nome: str, email: str, senha: str):
+    id_counter = 0
+    
+    def __init__(self, nome: str, email: str, senha: str, dataNasc: datetime, numeroTelefone: int, foto: str):
+        self.id = Usuario.id_counter
+        Usuario.id_counter += 1
         self.nome = nome
         self.email = email
         self.senha = senha
-        self.salvas: List['Sala'] = []
+        self.dataNasc = dataNasc
+        self.numeroTelefone = numeroTelefone
+        self.foto = foto
+        self.salvas: List['Sala'] = []  # Lista de salas salvas pelo usuário
 
     def __str__(self):
-        return f'{self.nome} ({self.email})'
+        return f'{self.nome} ({self.email}) ({self.id})'
+
+    def cadastrar_usuario(self, nome: str, email: str, senha: str) -> 'Usuario':
+        return Usuario(nome, email, senha, datetime.now(), 0, "")
+
+    @classmethod
+    def login(cls, email: str, senha: str, usuarios: List['Usuario']) -> Optional['Usuario']:
+        for usuario in usuarios:
+            if usuario.email == email and usuario.senha == senha:
+                return usuario
+        return None
+
+    def entrar_na_sala(self, sala: 'Sala') -> None:
+        if any(usuario.id == self.id for usuario in sala.participantes):
+            return
+        else:
+            sala.adicionar_participante(self)
+            print(f"Você entrou na sala '{sala.nome}'.")
 
 class Sala:
+    id_counter = 0
+    salas: List['Sala'] = []  # Lista de salas disponíveis no sistema (agora um atributo de classe)
+
     def __init__(self, nome: str, descricao: str, foto: str, criador: Usuario):
+        self.id = Sala.id_counter
+        Sala.id_counter += 1
         self.nome = nome
         self.descricao = descricao
         self.foto = foto
         self.criador = criador
-        self.status = SalaStatus.ATIVA
+        self.horarioCorte = datetime.now()
         self.participantes: List[Usuario] = [criador]
         self.locais_embarque = []
         self.locais_desembarque = []
 
     def __str__(self):
-        return f'Sala {self.nome} - Criada por {self.criador.nome}'
+        return f'Sala {self.nome} ({self.id}) - Criada por {self.criador.nome} ({self.criador.id})'
+
+    def adicionar_participante(self, usuario: Usuario):
+        self.participantes.append(usuario)
+
+    def remover_participante(self, usuario: Usuario):
+        if usuario in self.participantes:
+            self.participantes.remove(usuario)
 
     def adicionar_embarque(self, usuario: Usuario, local: str):
-        self.locais_embarque.append({"usuario": usuario.nome, "local": local})
+        self.locais_embarque.append({"id": usuario.id, "usuario": usuario.nome, "local": local})
 
     def adicionar_desembarque(self, usuario: Usuario, local: str):
-        self.locais_desembarque.append({"usuario": usuario.nome, "local": local})
+        self.locais_desembarque.append({"id": usuario.id, "usuario": usuario.nome, "local": local})
 
     def visualizar_locais(self):
-        print('')
-        print('='*30)
         print(f'\nLocais registrados na sala "{self.nome}":')
-        print('='*30)
-        print('')
         print("Embarques:")
-        print('')
         if self.locais_embarque:
             for embarque in self.locais_embarque:
                 print(f'Usuário: {embarque["usuario"]}, Local: {embarque["local"]}')
-            print('')
         else:
-            print('')
-            print('='*30)
             print("Nenhum local de embarque registrado.")
-            print('='*30)
-            print('')
-        print('='*30)
+        
         print("\nDesembarques:")
-        print('')
         if self.locais_desembarque:
             for desembarque in self.locais_desembarque:
                 print(f'Usuário: {desembarque["usuario"]}, Local: {desembarque["local"]}')
-            print('')
-            print('='*30)
         else:
-            print('')
-            print('='*30)
             print("Nenhum local de desembarque registrado.")
-            print('='*30)
-            print('')
 
-class Sistema:
-    def __init__(self):
-        self.usuarios: List[Usuario] = []
-        self.salas: List[Sala] = []
-        self.usuario_logado: Optional[Usuario] = None
+    def ver_participantes(self):
+        for participante in self.participantes:
+            print(participante)
 
-    def cadastrar_usuario(self, nome: str, email: str, senha: str):
-        usuario = Usuario(nome, email, senha)
-        self.usuarios.append(usuario)
-        print('')
-        print('='*34)
-        print(f'Usuário {usuario.nome} cadastrado com sucesso!')
-        print('='*34)
-        print('')
-        
-
-    def ver_usuarios(self):
-        if not self.usuarios:
-            print('')
-            print('='*30)
-            print("Nenhum usuário cadastrado.")
-            print('='*30)
-            print('')
-        else:
-            print('')
-            print('='*30)
-            print("Lista de usuários cadastrados:")
-            print('='*30)
-            print('')
-            for i, usuario in enumerate(self.usuarios, start=1):
-                print(f"{i}. Nome: {usuario.nome}, Email: {usuario.email}")
-                print('') 
-            print('='*30)
-            print('')
-
-    def login(self, email: str, senha: str):
-        for usuario in self.usuarios:
-            if usuario.email == email and usuario.senha == senha:
-                self.usuario_logado = usuario
-                print('')
-                print('='*30)
-                print(f'Bem-vindo(a), {usuario.nome}!')
-                print('='*30)
-                print('')
-                return True
-        print('')
-        print('='*30)
-        print('Credenciais inválidas!')
-        print('='*30)
-        print('')
-        return False
-
-    def criar_sala(self, nome: str, descricao: str, foto: str):
-        if self.usuario_logado:
-            sala = Sala(nome, descricao, foto, self.usuario_logado)
-            self.salas.append(sala)
-            self.usuario_logado.salvas.append(sala)
-            print('')
-            print('='*30)
-            print(f'Sala "{nome}" criada com sucesso!')
-            print('='*30)
-            print('')
-        else:
-            print('')
-            print('='*30)
-            print('Você precisa estar logado para criar uma sala.')
-            print('='*30)
-            print('')
-
-    def ver_salas(self):
-        if self.usuario_logado:
-            if not self.salas:
+    @staticmethod
+    def criar_sala(nome: str, descricao: str, foto: str, usuario_logado: Usuario) -> 'Sala':
+        sala = Sala(nome, descricao, foto, usuario_logado)
+        Sala.salas.append(sala)  # Adicionando a sala à lista de salas (agora um atributo de classe)
+        usuario_logado.salvas.append(sala)
+        return sala
+    
+    @staticmethod
+    def ver_salas(usuario_logado: Optional[Usuario]) -> None:
+        if usuario_logado:
+            if not Sala.salas:  # Acessando o atributo de classe diretamente
                 print('')
                 print('='*30)
                 print("Nenhuma sala disponível.")
@@ -148,121 +106,36 @@ class Sistema:
                 print('')
                 return None
 
-            
             print('')
             print('='*30)
             print("\nLista de Salas:")
             print('='*30)
             print('')
-            for i, sala in enumerate(self.salas, start=1):
-                print(f"{i}. Nome: {sala.nome}, Criador: {sala.criador.nome}")
+            for i, sala in enumerate(Sala.salas, start=1):
+                print(f"{i}. Id: {sala.id} Nome: {sala.nome}, Criador: {sala.criador.nome}")
             print('')
             print('='*30)
             print('')
-            return self.salas
+            return Sala.salas
         else:
             print('')
             print('='*30)
             print('Você precisa estar logado para visualizar as salas.')
             print('='*30)
             print('')
-
-    def entrar_na_sala(self, nome_sala: str):
-        for sala in self.salas:
-            if sala.nome == nome_sala:
-                sala.participantes.append(self.usuario_logado)
-                print('')
-                print('='*30)
-                print(f'Você entrou na sala "{sala.nome}".')
-                print('='*30)
-                print('')
-                return sala
-            print('')
-            print('='*30)
-            print("Sala não encontrada.")
-            print('='*30)
-            print('')
-        return None
-
-    def convidar_para_sala(self, sala: Sala):
+        
+    def convidar_para_sala(self):
         print('')
         print('='*30)
-        print(f"Link e código para convidar: {sala.nome}_CONVIDAR")
+        print(f"Link e código para convidar: {self.nome}_CONVIDAR")
         print('='*30)
         print('')
+    
 
-    def sair_da_sala(self, sala: Sala):
-        if sala and self.usuario_logado in sala.participantes:
-            sala.participantes.remove(self.usuario_logado)  
-            print('')
-            print('='*30)
-            print(f"Você saiu da sala '{sala.nome}'.")
-            print('='*30)
-            print('')
-        else:
-            print('')
-            print('='*30)
-            print("Você não está nesta sala.")
-            print('='*30)
-            print('')
+def menu():
+    usuarios: List[Usuario] = []
+    usuario_logado: Optional[Usuario] = None
 
-    def deletar_sala(self, sala: Sala):
-        if self.usuario_logado:
-            self.salas.remove(sala)
-            print('')
-            print('='*30)
-            print(f"Sala '{sala.nome}' deletada com sucesso!")
-            print('='*30)
-            print('')
-        else:
-            print('')
-            print('='*30)
-            print("Erro ao deletar a sala.")
-            print('='*30)
-            print('')
-
-    def marcar_local_embarque(self, sala: Sala, local: str):
-        if self.usuario_logado:
-            sala.adicionar_embarque(self.usuario_logado, local)
-            print('')
-            print('='*30)
-            print(f"Você marcou um local de embarque na sala '{sala.nome}'.")
-            print('='*30)
-            print('')
-        else:
-            print('')
-            print('='*30)
-            print("Você precisa estar logado para marcar um local de embarque.")
-            print('='*30)
-            print('')
-
-    def marcar_local_desembarque(self, sala: Sala, local: str):
-        if self.usuario_logado:
-            sala.adicionar_desembarque(self.usuario_logado, local) 
-            print('')
-            print('='*30)
-            print(f"Você marcou um local de desembarque na sala '{sala.nome}'.")
-            print('='*30)
-            print('')
-        else:
-            print('')
-            print('='*30)
-            print("Você precisa estar logado para marcar um local de desembarque.")
-            print('='*30)
-            print('')
-
-    def visualizar_todos_locais(self):
-        if self.salas:
-            for sala in self.salas:
-                sala.visualizar_locais()
-        else: 
-            print('')
-            print('='*30)
-            print("Nenhuma sala cadastrada.")
-            print('='*30)
-            print('')
-
-def menu(sistema):
     while True:
         print(('='*10),"Menu do Sistema: ",('='*10))
         print('')
@@ -283,7 +156,21 @@ def menu(sistema):
             nome = input("Nome: ")
             email = input("Email: ")
             senha = input("Senha: ")
-            sistema.cadastrar_usuario(nome, email, senha)
+            data_nasc = input("Data de Nascimento (formato: dd/mm/yyyy): ")
+            numero_telefone = input("Número de Telefone: ")
+            foto = input("Foto (link): ")
+            
+            data_nasc = datetime.strptime(data_nasc, "%d/%m/%Y")
+            numero_telefone = int(numero_telefone)
+
+            usuario = Usuario(nome, email, senha, data_nasc, numero_telefone, foto)
+            usuarios.append(usuario)
+            
+            print('')
+            print('='*34)
+            print(f'Usuário {usuario.nome} cadastrado com sucesso!')
+            print('='*34)
+            print('')
 
         elif escolha == '2':
             print('')
@@ -291,7 +178,13 @@ def menu(sistema):
             print("-> Ver Usuários")
             print('='*30)
             print('')
-            sistema.ver_usuarios()
+            if not usuarios:
+                print("Nenhum usuário cadastrado.")
+            else:
+                for i, usuario in enumerate(usuarios, start=1):
+                    print(f"{i}. Id: {usuario.id} Nome: {usuario.nome}, Email: {usuario.email}")
+            print('='*30)
+            print('')
 
         elif escolha == '3':
             print('')
@@ -301,25 +194,28 @@ def menu(sistema):
             print('')
             email = input("Email: ")
             senha = input("Senha: ")
-            sistema.login(email, senha)
+            usuario_logado = Usuario.login(email, senha, usuarios)
+            if usuario_logado:
+                print(f'Bem-vindo(a), {usuario_logado.nome}!')
+            else:
+                print('Credenciais inválidas!')
+            print('='*30)
 
         elif escolha == '4':
             print('')
             print('='*30)
-            print("-> Criar sala")
+            print("-> Criar Sala")
             print('='*30)
             print('')
-            if sistema.usuario_logado:
+            if usuario_logado:
                 nome = input("Nome da sala: ")
                 descricao = input("Descrição: ")
                 foto = input("Foto (link): ")
-                sistema.criar_sala(nome, descricao, foto)
-            else: 
-                print('')
-                print('='*30)
+                sala = Sala.criar_sala(nome, descricao, foto, usuario_logado)
+                print(f'Sala "{nome}" criada com sucesso!')
+            else:
                 print('Você precisa estar logado para criar uma sala.')
-                print('='*30)
-                print('')
+            print('='*30)
 
         elif escolha == '5':
             print('')
@@ -327,20 +223,21 @@ def menu(sistema):
             print("-> Ver salas")
             print('='*30)
             print('')
-            salas = sistema.ver_salas()
+            salas = Sala.ver_salas(usuario_logado)
             if salas:
                 print("\n1. Entrar na Sala")
                 print("0. Sair")
                 print('')
                 sub_escolha = input("Escolha uma opção: ")
-                print('')
 
                 if sub_escolha == '1':
                     print('')
                     nome_sala = input("Digite o nome da sala: ")
                     print('')
-                    sala = sistema.entrar_na_sala(nome_sala)
-                    if sala:
+                    sala_encontrada = next((sala for sala in salas if sala.nome == nome_sala), None)
+                    if sala_encontrada:
+                        usuario_logado.entrar_na_sala(sala_encontrada)
+                        print(f'Você entrou na sala "{sala_encontrada.nome}".')
                         while True:
                             print('')
                             print("1. Convidar para Sala")
@@ -349,36 +246,40 @@ def menu(sistema):
                             print("4. Marcar Local de Embarque")
                             print("5. Marcar Local de Desembarque")
                             print("6. Mostrar Locais")
+                            print("7. Mostrar Participantes")
                             print("0. Sair")
                             print('')
                             sub_sub_escolha = input("Escolha uma opção: ")
 
                             if sub_sub_escolha == '1':
-                                sistema.convidar_para_sala(sala)
+                                sala_encontrada.convidar_para_sala()
                             elif sub_sub_escolha == '2':
-                                sistema.sair_da_sala(sala)
+                                sala_encontrada.remover_participante(usuario_logado)
                                 break
                             elif sub_sub_escolha == '3':
-                                sistema.deletar_sala(sala)
+                                salas.remove(sala_encontrada)
                                 break
                             elif sub_sub_escolha == '4':
-                                print('')
                                 local = input("Digite o local de embarque: ")
-                                print('')
-                                sistema.marcar_local_embarque(sala, local)
+                                sala_encontrada.adicionar_embarque(usuario_logado, local)
                             elif sub_sub_escolha == '5':
-                                print('')
                                 local = input("Digite o local de desembarque: ")
-                                print('')
-                                sistema.marcar_local_desembarque(sala, local)
+                                sala_encontrada.adicionar_desembarque(usuario_logado, local)
                             elif sub_sub_escolha == '6':
-                                sistema.visualizar_todos_locais()
+                                sala_encontrada.visualizar_locais()
+                            elif sub_sub_escolha == '7':
+                                sala_encontrada.ver_participantes()
                             elif sub_sub_escolha == '0':
                                 break
                             else:
                                 print("Opção inválida!")
+                    else:
+                        print("Sala não encontrada.")
                 elif sub_escolha == '0':
                     continue
+            else:
+                print("Nenhuma sala disponível.")
+            print('='*30)
 
         else:
             print('')
@@ -388,5 +289,4 @@ def menu(sistema):
             print('')
 
 if __name__ == "__main__":
-    sistema = Sistema()
-    menu(sistema)
+    menu()
